a5271c250b89c3bdf727369d2fb61e2d07ad2814
m.jamalpour <mohsenjamal1023@gmail.com>
2025-07-09 18:32:35 +0330

    change header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaFlask,FaClipboardList } from 'react-icons/fa';
-
+import { FaFlask, FaClipboardList } from 'react-icons/fa';
 
 export default function HomePage() {
   return (
     <>
-      <h1 className="text-2xl font-bold flex justify-center mb-5">راهنمای جامع پرستاری</h1>
+      <h1 className="text-3xl font-extrabold text-white bg-gradient-to-r from-blue-600 to-cyan-400 p-4 rounded-xl shadow-md text-center mb-8">
+        راهنمای جامع پرستاری
+      </h1>
       <div>
-        <div className=" mr-2 flex items-center flex-row">
-         <FaFlask size={20} className="text-[#4A90E2]" />
-      <Link className="mr-1 text-[#366EBD] font-bold" to="/lab">آزمایشات</Link>  
-
+        <div className="mr-2 flex items-center flex-row">
+          <FaFlask size={20} className="text-[#4A90E2]" />
+          <Link className="mr-1 text-[#366EBD] font-bold" to="/lab">
+            آزمایشات
+          </Link>
         </div>
         <div className='mt-5 mr-2 flex items-center flex-row'>
-           <FaClipboardList size={20} className="text-[#4A90E2]" />
-
-      <Link  className="mr-1  text-[#366EBD] font-bold" to="/nursingdiagnosis">تشخیص های پرستاری</Link>  
+          <FaClipboardList size={20} className="text-[#4A90E2]" />
+          <Link className="mr-1 text-[#366EBD] font-bold" to="/nursingdiagnosis">
+            تشخیص های پرستاری
+          </Link>
         </div>
-
       </div>
-
     </>
   )
 }
