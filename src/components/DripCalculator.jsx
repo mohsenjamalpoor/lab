b80c4980c8856274df5172e65d83984335317ad4
m.jamalpour <mohsenjamal1023@gmail.com>
2025-07-13 12:24:46 +0330

    add mannitol

--- src/components/DripCalculator.jsx
+++ src/components/DripCalculator.jsx
@@ -1,14 +1,19 @@
 
 import React, { useState } from 'react';
+import { IoChevronBackCircle } from 'react-icons/io5';
+import { useNavigate } from 'react-router-dom';
 
 const drugOptions = [
   { name: 'داروی عمومی', value: 'general' },
   { name: 'هپارین', value: 'heparin' },
   { name: 'لابتولول', value: 'labetalol' },
+  { name: 'مانیتول', value: 'mannitol' },
+
 
 ];
 
 export default function DripCalculator() {
+  const navigator = useNavigate();
   const [drugType, setDrugType] = useState('');
   const [totalVolume, setTotalVolume] = useState('');
   const [totalMedical, setTotalMedical] = useState('');
@@ -20,19 +25,22 @@ export default function DripCalculator() {
     heparin: () => {
       const divisor = ampouleCount === '1' ? 100 : 200;
       const rate = parseFloat(doctorOrder) / divisor;
-      return ` سرعت تزریق هپارین: ${rate.toFixed(2)} ml/hr`;
+      return ` سرعت تزریق هپارین: ml/hr ${rate.toFixed(2)} `;
     },
 
     labetalol: () => {
       const rate = parseFloat(doctorOrder) / 5;
-      return ` سرعت تزریق لابتولول: ${rate.toFixed(2)} ml/hr`;
+      return `سرعت تزریق لابتولول: ml/hr ${rate.toFixed(2)} `;
     },
 
-   
+    mannitol: () => {
+      const rate = parseFloat(doctorOrder) * 5;
+      return ` مانیتول:  ${rate.toFixed(2)} cc `;
+    },
 
     general: () => {
       const rate = (parseFloat(totalVolume) * parseFloat(doctorOrder)) / parseFloat(totalMedical);
-      return ` سرعت انفوزیون: ${rate.toFixed(2)} ml/hr`;
+      return ` سرعت انفوزیون: ml/hr ${rate.toFixed(2)} `;
     },
   };
 
@@ -56,6 +64,10 @@ export default function DripCalculator() {
 
   return (
     <div className="max-w-md mx-auto bg-white p-6 rounded-xl shadow-md mt-5">
+       <IoChevronBackCircle
+              onClick={() => navigator(-1)}
+              className="absolute top-4 left-4 text-3xl text-blue-600 cursor-pointer hover:scale-105 transition-transform"
+            />
       <h2 className="text-xl font-bold mb-4 text-center text-blue-700">محاسبه سرعت انفوزیون</h2>
 
       <div className="mb-4">
@@ -106,6 +118,17 @@ export default function DripCalculator() {
       {drugType === 'labetalol' && (
         <div className="mb-4">
           <label className="block text-gray-700">دستور پزشک (mg/min):</label>
+          <input
+            type="number"
+            className="w-full border rounded p-2 mt-1"
+            value={doctorOrder}
+            onChange={(e) => setDoctorOrder(e.target.value)}
+          />
+        </div>
+      )}
+        {drugType === 'mannitol' && (
+        <div className="mb-4">
+          <label className="block text-gray-700">دستور پزشک (gr):</label>
           <input
             type="number"
             className="w-full border rounded p-2 mt-1"
