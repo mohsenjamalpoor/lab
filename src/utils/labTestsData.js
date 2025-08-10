b089585e1f7ea3c966a315301a776ecb5c6840ac
m.jamalpour <mohsenjamal1023@gmail.com>
2025-08-10 11:23:06 +0330

    add new lab

--- src/utils/labTestsData.js
+++ src/utils/labTestsData.js
@@ -65,7 +65,7 @@ const labTestsData = [
     tubeType: "PT",
     normalRange: "",
   },
-   {
+  {
     testName: "Protein s",
     tubeType: "PT",
     normalRange: "",
@@ -80,47 +80,47 @@ const labTestsData = [
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Cr",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "BUN",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "CPK",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "RF",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "VDRL",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Wright",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "WIDAL",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Cold agglutinin",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Coombs wright",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
@@ -135,33 +135,33 @@ const labTestsData = [
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
-    testName:"Ph",
+  {
+    testName: "Ph",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
-    testName:"SGOT",
+  {
+    testName: "SGOT",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
-    testName:"SGPT",
+  {
+    testName: "SGPT",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
-    testName:"T4",
+  {
+    testName: "T4",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
-    testName:"T3",
+  {
+    testName: "T3",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
-    testName:"TSH",
+  {
+    testName: "TSH",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
@@ -180,92 +180,92 @@ const labTestsData = [
     tubeType: "cbc",
     normalRange: "",
   },
-   {
+  {
     testName: "Lactate",
     tubeType: " cbc روی یخ",
     normalRange: "",
   },
-   {
+  {
     testName: "Fibrinogen",
     tubeType: "PT",
     normalRange: "",
   },
-   {
+  {
     testName: "Anti thrombin lll",
     tubeType: "PT",
     normalRange: "",
   },
-   {
+  {
     testName: "Factor v Leidon",
     tubeType: "PT",
     normalRange: "",
   },
-   {
+  {
     testName: "اتانول- متانول",
     tubeType: "PT",
     normalRange: "",
   },
-   {
+  {
     testName: "Factor H/B/I",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "G6PD",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "Hb Electrophoresis",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "Pyruvate",
     tubeType: "داخل لوله شیشه ای مخصوص از آزمایشگاه تهیه می شود",
     normalRange: "",
   },
-    {
+  {
     testName: "HB A1C",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "ACTH",
     tubeType: "cbc روی یخ",
     normalRange: "",
   },
-    {
+  {
     testName: "NBT",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "CMV Ag",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "سیرولیموس(راپامیون)",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "PBS",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "Amylase",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "B1- تیامین",
     tubeType: "cbc",
     normalRange: "",
   },
-    {
+  {
     testName: "G6PD",
     tubeType: "cbc",
     normalRange: "",
@@ -274,7 +274,8 @@ const labTestsData = [
     testName: "Lipase",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
-  },{
+  },
+  {
     testName: "GAMA GT",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
@@ -294,44 +295,46 @@ const labTestsData = [
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "Dir.Coombs",
     tubeType: "cbc",
     normalRange: "",
   },
-   {
+  {
     testName: "Tacrolimus Level",
     tubeType: "cbc",
     normalRange: "",
   },
-   {
+  {
     testName: "Factor v PCR",
     tubeType: "cbcدو عدد",
     normalRange: "",
   },
-   {
+  {
     testName: "Amino acid blood HPLC/کروماتوگرافی اسید های امینه",
     tubeType: "cbc",
     normalRange: "",
   },
-   {
+  {
     testName: "PCR LP",
     tubeType: "نمونه LP",
     normalRange: "",
   },
-   {
-    testName: "Urine amine TLC/Urine sugar TLC/کروماتوگرافی قندو یا اسید امینه ادرار",
+  {
+    testName:
+      "Urine amine TLC/Urine sugar TLC/کروماتوگرافی قندو یا اسید امینه ادرار",
     tubeType: "10cc ادرار",
     normalRange: "",
   },
-   {
+  {
     testName: "Protein electrophoresis",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "Adamts",
-    tubeType: "2 نمونهcbc/pt/بیوشیمی   Bun/Cr/cbc-diff/CD46/Retic/Adamts13/Anti Adamts13/FactorB/FactorH/Factor l",
+    tubeType:
+      "2 نمونهcbc/pt/بیوشیمی   Bun/Cr/cbc-diff/CD46/Retic/Adamts13/Anti Adamts13/FactorB/FactorH/Factor l",
     normalRange: "",
   },
   {
@@ -339,127 +342,127 @@ const labTestsData = [
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Direct bili",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Total bili",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "Albumin",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "TIBC",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "CL",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "Uric acide",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "2HPP",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "Globulin",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "25 OH Vit D3",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "Ferritin",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "C3",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "C4",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "IgM/IgA/IgG",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "HIV",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "HCV Ag",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-   {
+  {
     testName: "HBS Ag",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
-    {
+  {
     testName: "K-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "LDH-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Amylase-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Albumin-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "ALK-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Bilirobin-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "BUN-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Cholestrol-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Cr-F",
     tubeType: "مایعات بدن",
     normalRange: "",
@@ -469,169 +472,503 @@ const labTestsData = [
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Glubulin-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "NA-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Protein-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "Triglyceride-F",
     tubeType: "مایعات بدن",
     normalRange: "",
   },
-   {
+  {
     testName: "U/A",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-    {
+  {
     testName: "U/C",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Urine SG",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Urine R.S",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Keton",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Urine protein",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Urine Glucose",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-   {
+  {
     testName: "Phزیر پارافین",
     tubeType: "2cc نمونه ادرار ",
     normalRange: "",
   },
-    {
+  {
     testName: "CSFروتین",
     tubeType: "مایع مغزی -نخاعی",
     normalRange: "",
   },
-    {
+  {
     testName: "Pleural",
     tubeType: "2cc نمونه داخل لوله شیشیه استریل ",
     normalRange: "",
   },
-    {
+  {
     testName: "Asiteروتین",
     tubeType: "2cc نمونه داخل لوله شیشیه استریل ",
     normalRange: "",
   },
-    {
+  {
     testName: "Dialysisروتین",
     tubeType: "نمونه مایع دیالیز استریل",
     normalRange: "",
   },
-    {
+  {
     testName: "Borrelia-Malaria",
     tubeType: "cbc",
     normalRange: "",
   },
-   {
+  {
     testName: "ESR",
     tubeType: "ESR-cbc",
     normalRange: "",
   },
-   {
-    testName:"S/E",
+  {
+    testName: "S/E",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"S/c",
+  {
+    testName: "S/c",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"Fat drop",
+  {
+    testName: "Fat drop",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"OB",
+  {
+    testName: "OB",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"Ph stool",
+  {
+    testName: "Ph stool",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"RS stool",
+  {
+    testName: "RS stool",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"Scotch test",
+  {
+    testName: "Scotch test",
     tubeType: "نمونه مدفوع",
     normalRange: "",
   },
-   {
-    testName:"Blood Group",
+  {
+    testName: "Blood Group",
     tubeType: "cbc",
     normalRange: "",
   },
-   {
-    testName:"TORCH",
-    tubeType: "5cc خون لخته   /   HSV(G/M) / CMV(G/M)  / Rubella(G/M) / Toxo(G/M)",
+  {
+    testName: "TORCH",
+    tubeType:
+      "5cc خون لخته   /   HSV(G/M) / CMV(G/M)  / Rubella(G/M) / Toxo(G/M)",
     normalRange: "",
   },
-    {
-    testName:"ANA(I.F)",
-    tubeType: "5cc خون لخته   /   P.ANCA  /  ANCA  /  C.ANCA  /  Anti RP3  /  Anti ds DNA   /  Anti MPO  /  ANA(I.F)",
+  {
+    testName: "ANA(I.F)",
+    tubeType:
+      "5cc خون لخته   /   P.ANCA  /  ANCA  /  C.ANCA  /  Anti RP3  /  Anti ds DNA   /  Anti MPO  /  ANA(I.F)",
     normalRange: "",
   },
-    {
-    testName:"Galactomannan",
+  {
+    testName: "Galactomannan",
     tubeType: "cbcروی یخ",
     normalRange: "",
   },
-    {
-    testName:"HLA ABC(IGRA)",
+  {
+    testName: "HLA ABC(IGRA)",
     tubeType: "لوله DHR",
     normalRange: "",
   },
-    {
-    testName:"Topiramate",
+  {
+    testName: "Topiramate",
     tubeType: "لوله بیوشیمی",
     normalRange: "",
   },
+  {
+    testName: "VZV PCR",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "VZV IgM",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "VZV IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "VWF:Ag",
+    tubeType: " pt,cbc,لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "VWF:Act",
+    tubeType: " pt,cbc,لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "VMA 24hr",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "VMA",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Vitamin A",
+    tubeType: " cbc,لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Vit 12",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "ٰVancomycin level",
+    tubeType: " cbc,لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Valporat-NA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Urine Sugar TLC",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
 
- 
- 
- 
- 
- 
- 
-
-
+  {
+    testName: "Urine Protein Electrophoresis",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Urine Amino TLC",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Urin Random Cystatin C",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. Cystine 24hr",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur.Cystatin C/24hrs",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. Tyrosine",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. Random CU",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. Homosystine",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. Cystine",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur. cu/24 hrs",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Ur micro albumin 24h",
+    tubeType: "نمونه ادرار",
+    normalRange: "",
+  },
+  {
+    testName: "Troponin",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Transferin Saturation",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Transferin",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Toxo(IgM)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Toxo(IgG)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Thyroglobulin",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Tetanus IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Testosterone",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Tacrolimus Level",
+    tubeType: "cbc",
+    normalRange: "",
+  },
+  {
+    testName: "ACE",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "ACE level",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "ADA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "ADH",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Aldolase",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Aldosterone",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Alpha heavy chain",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Alpha 1 anti trypsin",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti B2 Glycoprotein",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti tTG IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti Cardiolipine(IgG)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti Cardiolipine(IgM)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti CCP",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti G.B.M",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti gliadinIgA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti gliadinIgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti Leishmania IgA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti Leishmania IgM",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti Leishmania IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti LKM",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti MPO",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti phospholipid IgM",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti phospholipid IgA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti smith Ab",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti TG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti gliadin IgA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti gliadin IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti gliadin IgM",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "ASMA",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "AntiCardiolipine(IgM)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "AntiCardiolipine(IgG)",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti TPO",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "Anti tTG IgG",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
+  {
+    testName: "",
+    tubeType: "لوله بیوشیمی",
+    normalRange: "",
+  },
 ];
 export default labTestsData;
